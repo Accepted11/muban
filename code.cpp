3fc797484d9e7f4893b804e13984239ce88b0617
Accepted11 <54001386+Accepted11@users.noreply.github.com>
2019-08-22 17:48:58 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,53 +1,37 @@
-#include <cstdio>
-#include <cstring>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
-map<int,int>mp;
-int n,m,s,t,k;
-struct node {
-    int a[205][205];
-    node operator*(const node &b) const {
-        node res;
-        memset(res.a,0x3f, sizeof(res.a));
+const int maxn=1010;
+int a[maxn],b[maxn],f[maxn][maxn],n,m,ans;
+int main() {
+    while (scanf("%d%d", &n, &m)) {
+        ans = 0x3f3f3f3f;
+        if (n == 0 && m == 0) break;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= n; k++) {
-                    res.a[i][j] = min(res.a[i][j],a[i][k] + b.a[k][j]);
+            scanf("%d", &a[i]);
+            a[i]--;
+        }
+        for (int i = 1; i <= n; i++) {
+            scanf("%d", &b[i]);
+            b[i]--;
+            a[i] = (b[i] - a[i] + m) % m;
+        }
+        memset(f, 0x3f, sizeof(f));
+        f[1][0] = a[1];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (a[i] >= a[i + 1]) {
+                    f[i + 1][j] = min(f[i + 1][j], f[i][j]);
+                    if (j + 1 <= n)
+                        f[i + 1][j + 1] = min(f[i + 1][j + 1], f[i][j] + (a[i + 1] + (j + 1) * m) - (a[i] + j * m));
+                } else {
+                    if (j) f[i + 1][j - 1] = min(f[i + 1][j - 1], f[i][j]);
+                    f[i + 1][j] = min(f[i + 1][j], f[i][j] + (a[i + 1] + j * m) - (a[i] + j * m));
                 }
             }
         }
-        return res;
-    }
-}st;
-node pow(node b,int c){
-    node res;
-    for (int i=1;i<=n;i++) {
-        for (int j = 1; j <= n; j++) {
-            res.a[i][i] = 0;
+        for (int i = 0; i <= n; i++) {
+            ans = min(ans, f[n][i]);
         }
+        printf("%d\n", ans);
     }
-    for (int i=1;i<=n;i++){
-        res.a[i][i]=1;
-    }
-    while (c) {
-        if (c & 1) {
-            res = res * b;
-        }
-        c >>= 1;
-        b = b * b;
-    }
-    return res;
-}
-int main() {
-    scanf("%d%d%d%d", &k, &m, &s, &t);
-    memset(st.a, 0x3f, sizeof(st.a));
-    while (m--) {
-        int x, y, z;
-        scanf("%d%d%d", &z, &x, &y);
-        if (mp[x]) x = mp[x]; else x = mp[x] = ++n;
-        if (mp[y]) y = mp[y]; else y = mp[y] = ++n;
-        st.a[x][y] = z;
-    }
-    node ans = pow(st, k);
-    printf("%d\n", ans.a[mp[s]][mp[t]]);
 }
