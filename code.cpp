58a5c73630da92fa23c0543575d00d42e2dbb999
Accepted11 <54001386+Accepted11@users.noreply.github.com>
2019-10-10 09:40:18 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,51 +1,53 @@
+#include <bits/stdc++.h>
 
-const int maxn=50010;
-const int DIM=10;
-inline double sqr(double)
-{
-    return x*x;
-}
-namespace KDTtree
-{
-int k;
+using namespace std;
+const int maxn=5e5+10;
+string outx,outy,userx,usery;
 struct Point
 {
-    int x[DIM];
-    double distance(const Point &b)const
+    int x,y;
+} a[maxn];
+int n,r;
+int main(int argc,char *args[])
+{
+    ifstream user,in,out;
+    in.open(args[1]);
+    out.open(args[2]);
+    user.open(args[3]);
+    in>>n;
+    in>>r;
+    for (int i=1; i<=n; i++)
     {
-        double ret=0;
-        for (int i=0; i<k; i++)
-            ret+=sqr(x[i]-b.x[i]);
-        return ret;
+        in>>a[i].x>>a[i].y;
     }
-    void input()
+    out>>outx;
+    if (outx=="-1")
     {
-        for (int i=0; i<k; i++)
-            scanf("%d",&x[i]);
+        user>>userx;
+        if (userx=="-1")
+            return 1;
+        else
+            return 0;
     }
-    void output()
-    {
-        for (int i=0; i<k; i++)
-        {
-            printf("%d",x[i]);
-            if (i==k-1)
-                printf("\n");
-            else
-                printf(" ");
-        }
+    out>>outy;
+    user>>userx>>usery;
+    int len1=outx.length();
+    int len2=outy.length();
+    int len3=userx.length();
+    int len4=usery.length();
+    if (len1>100||len2>100||len3>100||len4>100)
+        return 0;
+    for (int i=0;i<len1;i++){
+        if (outx[i]=='.') return 0;
     }
-    struct qnode
-    {
-        Point p;
-        double dis;
-        qonde(){};
-        qonde(Point _p,double _dis):p(_p),dis(_dis){}
-        bool operator <(const qonde &b) const{
-            return dis<b.dis;
-        }
-    };
-    priority_queue<qnode>q;
-    struct cmpx{
-        int div;
-        cmpx()
-    };
+    for (int i=0;i<len2;i++){
+        if (outy[i]=='.') return 0;
+    }
+    for (int i=0;i<len1;i++){
+        if (outx[i]=='.') return 0;
+    }
+
+    in.close();
+    out.close();
+    user.close();
+}
