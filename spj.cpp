45e0131bc1f63e84f826e1a5de5057df0efb6a70
Amber0130 <54001386+Amber0130@users.noreply.github.com>
2021-03-06 22:35:12 +0800

    Update spj.cpp

--- spj.cpp
+++ spj.cpp
@@ -39,39 +39,6 @@ int main(int argc,char *args[]) {
         fclose(f_user);
         return ret;
     }
-    for (int i = 1; i <= n; i++) {
-        fscanf(f_user, "%d%d", &a[i].l, &a[i].r);
-        if (a[i].l >= a[i].r || a[i].l < 1 || a[i].r > 1e9) {
-            ret = 1;
-            fclose(f_in);
-            fclose(f_out);
-            fclose(f_user);
-            return ret;
-        }
-        if (!ma.count(a[i].l)) {
-            ma[a[i].l] = 1;
-        } else {
-            ret = 1;
-            fclose(f_in);
-            fclose(f_out);
-            fclose(f_user);
-            return ret;
-        }
-        if (!ma.count(a[i].r)) {
-            ma[a[i].r] = 1;
-        } else {
-            ret = 1;
-            fclose(f_in);
-            fclose(f_out);
-            fclose(f_user);
-            return ret;
-        }
-    }
-    sort(a + 1, a + n + 1, cmp1);
-    ans1 = solve();
-    sort(a + 1, a + n + 1, cmp2);
-    ans2 = solve();
-    if (ans1 - ans2 != m) ret = 1;
     fclose(f_in);
     fclose(f_out);
     fclose(f_user);
